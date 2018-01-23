fe8321ceabe1de57b56dd9a4f4af5042be83e2fc
Ragesh Shunmugam <ragesh.shunmugam@ebpi.nl>
2018-01-23 15:47:36 +0100

    changed to bootstrap.xml

--- cloudconfigclient/src/main/java/com/techjava/cloudconfigclient/CloudconfigclientApplication.java
+++ cloudconfigclient/src/main/java/com/techjava/cloudconfigclient/CloudconfigclientApplication.java
@@ -1,9 +1,12 @@
 package com.techjava.cloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
+@EnableWebMvc
+@EnableAutoConfiguration
 @SpringBootApplication
 public class CloudconfigclientApplication {
 
--- cloudconfigclient/src/main/java/com/techjava/cloudconfigclient/controller/EnvController.java
+++ cloudconfigclient/src/main/java/com/techjava/cloudconfigclient/controller/EnvController.java
@@ -3,13 +3,15 @@ package com.techjava.cloudconfigclient.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RefreshScope
+@RequestMapping("/rest")
+@RestController
 public class EnvController {
 
-    @Value("${msg:Config server is down. Sorry for inconvenience}")
+    @Value("${msg: Config server is down. Sorry for inconvenience..!}")
     private String msg;
 
     @GetMapping("/msg")
